aca64b59fc03c59a38d0d7f922601adab6b8e6c2
oralsiraj-coder <o.r.alsiraj@gmail.com>
2026-05-04 16:31:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -252,7 +252,7 @@ st.success(f"Kp = {Kp:.4f}")
 # =======================
 # EFFECTIVE VERTICAL STRESS – THEORY
 # =======================
-
+st.header("Vertical stress calculation")
 st.markdown(
     """
     **Effective vertical stress** represents the portion of the total stress
