8be74be9baf5108704a564bdbaffa6a2fed9d96b
oralsiraj-coder <o.r.alsiraj@gmail.com>
2026-05-06 15:25:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -354,7 +354,7 @@ ax_eff.set_title("Effective Vertical Stress Distribution")
 ax_eff.grid(True, linestyle=":", linewidth=0.7)
 ax_eff.legend(loc="best")
 #ax_eff.set_xlim(left=min(sigma_v_water.min(), 0))  # ensures negative values are visible
-
+ax_eff.set_ylim(0, z.min())
 # ---- Reduce empty margins ----
 ax_eff.margins(x=0.05, y=0)
 
