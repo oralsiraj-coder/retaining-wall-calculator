92a0523cca2fd3067a3aca2f80899d466992b8b5
oralsiraj-coder <o.r.alsiraj@gmail.com>
2026-05-06 10:28:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -194,58 +194,7 @@ st.pyplot(draw_wall(
     gamma_p, phi_p, c_p
 ))
 #================================================================================================================#
-st.markdown("""
-<style>
-.katex-display {
-    text-align: left !important;
-    margin-left: 0 !important;
-}
-</style>
-""", unsafe_allow_html=True)
-# =======================
-# Ka (ACTIVE)
-# =======================
 
-st.latex(rf"""
-\begin{{aligned}}
-
-K_a &= \cos\beta \cdot 
-\frac{{\cos\beta - \sqrt{{\cos^2\beta - \cos^2\varphi}}}}
-{{\cos\beta + \sqrt{{\cos^2\beta - \cos^2\varphi}}}} \\[6pt]
-
-K_a &= \cos({beta:.1f}^\circ) \cdot
-\frac{{\cos({beta:.1f}^\circ) - \sqrt{{\cos^2({beta:.1f}^\circ) - \cos^2({phi_a:.1f}^\circ)}}}}
-{{\cos({beta:.1f}^\circ) + \sqrt{{\cos^2({beta:.1f}^\circ) - \cos^2({phi_a:.1f}^\circ)}}}} \\[6pt]
-
-K_a &= {Ka:.4f}
-
-\end{{aligned}}
-""")
-
-st.success(f"Ka = {Ka:.4f}")
-
-# =======================
-# Kp (PASSIVE)
-# =======================
-
-st.latex(rf"""
-\begin{{aligned}}
-
-K_p &= \cos\beta \cdot 
-\frac{{\cos\beta + \sqrt{{\cos^2\beta - \cos^2\varphi}}}}
-{{\cos\beta - \sqrt{{\cos^2\beta - \cos^2\varphi}}}} \\[6pt]
-
-K_p &= \cos(0^\circ) \cdot
-\frac{{\cos(0^\circ) + \sqrt{{\cos^2(0^\circ) - \cos^2({phi_p:.1f}^\circ)}}}}
-{{\cos(0^\circ) - \sqrt{{\cos^2(0^\circ) - \cos^2({phi_p:.1f}^\circ)}}}} \\[6pt]
-
-K_p &= {Kp:.4f}
-
-\end{{aligned}}
-""")
-
-st.success(f"Kp = {Kp:.4f}")
-``
 #=============================================================================
 # =======================
 # Ka (ACTIVE)
