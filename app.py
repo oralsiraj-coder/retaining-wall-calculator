53ff8d039f17460bbfe9feafd3e414eb73623df7
oralsiraj-coder <o.r.alsiraj@gmail.com>
2026-05-07 14:45:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -341,13 +341,13 @@ fig_eff, ax_eff = plt.subplots(figsize=(6, 5))
 ax_eff.plot(sigma_v_soil, z, linestyle="--", color="brown", label="Soil self-weight")
 ax_eff.plot(sigma_v_water, z, linestyle="--", color="cyan", label="Water")
 ax_eff.plot(sigma_v_surcharge, z, linestyle="--", color="green", label="surcharge")
-ax_eff.plot(sigma_v_effective, z, linestyle="--", color="blue", label="Effective soil")
+ax_eff.plot(sigma_v_effective, z, linestyle="--", color="blue", label="Effective stress ")
 
 # ---- Effective stress (main curve) ----
 ax_eff.plot(
     sigma_v_effective_total, z,
     linewidth=2.5, color="black",
-    label="Effective stress σ′ᵥ"
+    label="Toral effective stress"
 )
 
 # ---- Reference lines ----
