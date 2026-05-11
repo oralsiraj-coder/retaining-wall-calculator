f2c744d9310d50b2e910ec5827ccdcc6e0799ac7
oralsiraj-coder <o.r.alsiraj@gmail.com>
2026-05-11 14:19:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -694,7 +694,8 @@ st.latex(r"P_{a1} = \frac{1}{2} K_a \gamma_a H_a^2")
 st.latex(
     rf"P_{{a1}} = \frac{{1}}{{2}} \cdot {Ka:.3f} \cdot {gamma_a:.1f} \cdot {Ha:.1f}^2") # render the symboles 
 # =================== Camupute values ==========================
-st.latex(= {Pa1:.2f}")
+st.latex(rf"P_{{a1}} = {Pa1:.2f}")
+
 
 
 
