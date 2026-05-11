8adf6fb2980704795c3ca98fac2c48e9d137d55c
oralsiraj-coder <o.r.alsiraj@gmail.com>
2026-05-11 14:39:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -698,7 +698,7 @@ st.latex(rf"y_{{a1}} = {Ha/3:.2f}")
 st.subheader("2. Horizontal force due to water table (Pa₂)")
 
 st.latex(r"P_{a2} = \frac{1}{2} \gamma_w (H_a-H_w)^2")
-
+st.latex(  rf"P_{{a2}} = \frac{{1}}{{2}} \cdot {gamma_w:.2f} \cdot ({Ha:.1f} - {Hw:.1f})^2")
 st.latex(r"y_{a2} = \frac{(H_a-H_w)}{3}")
 
 # --------------------------------------------------
