a2204687ae848eb97d1d9b43c170dc493c2024a0
oralsiraj-coder <o.r.alsiraj@gmail.com>
2026-05-11 14:05:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -683,14 +683,11 @@ st.header("Earth Pressure Resultants")
 # --------------------------------------------------
 # Pa1 – Soil
 # --------------------------------------------------
-st.subheader("1. Horizontal force due to soil (Pa₁)")
-st.latex(r"P_{a1} = \frac{1}{2} K_a \gamma_a H_a^2")
+st.subheader("1. Horizontal force due to soil (Pa₁)")    # render the symboles 
+st.latex(r"P_{a1} = \frac{1}{2} K_a \gamma_a H_a^2")     # render the numbers
 st.latex(
     rf"P_{{a1}} = \frac{{1}}{{2}} \cdot {Ka:.3f} \cdot {gamma_a:.1f} \cdot {Ha:.1f}^2"
-)
-st.latex(
-    rf"P_{{a1}} = \frac{{1}}{{2}} \cdot {Ka:.3f} \cdot {gamma_a:.1f} \cdot {Ha:.1f}^2 = {Pa1:.2f}"
-)
+
 st.latex(r"P_{a1} = \frac{1}{2} K_a \gamma_a H_a^2")
 
 st.latex(r"y_{a1} = \frac{H_a}{3}")
