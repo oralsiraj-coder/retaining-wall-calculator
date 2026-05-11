bb4d8cbcee26a13b389803eb4abc6cdbc20cdb27
oralsiraj-coder <o.r.alsiraj@gmail.com>
2026-05-11 09:19:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -683,7 +683,7 @@ def draw_wall(Ha, Hw, Hp, Th, Lh, Lt, Tsb, beta,
     # =======================
     if z is not None and sigma_h is not None:
 
-        x_wall = x0 + Lh_s/2
+        x_wall = x0
         y_top = y0 + Th_s
 
         # Scale stresses for visualization
