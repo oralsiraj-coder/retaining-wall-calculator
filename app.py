2833981ad81fa2e69b3057758d264dbed2d1875a
oralsiraj-coder <o.r.alsiraj@gmail.com>
2026-05-12 15:36:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -688,6 +688,12 @@ st.header("Earth Pressure Resultants")
 # --------------------------------------------------
 st.subheader("1. Horizontal force due to soil (Pa₁)")    
 Pa1 = 0.5 * Ka * gamma_a * Ha**2
+y_a1 = H_a/3
+y_a2 = (H_a-H_w)/3
+y_a3 = H_a/2
+
+M_overturning = Pa1 * y_a1+ Pa2 * y_a2 + Pa3 * y_a3
+
 st.latex(r"P_{a1} = \frac{1}{2} K_a \gamma_a H_a^2")      
 st.latex( rf"P_{{a1}} = \frac{{1}}{{2}} \cdot {Ka:.3f} \cdot {gamma_a:.1f} \cdot {Ha:.1f}^2 = {Pa1:.2f}")
 st.latex(rf"P_{{a1}} = {Pa1:.2f}")
@@ -913,6 +919,11 @@ M2 = W2 * X_W_2
 M3 = W3 * X_W_3
 M4 = W4 * X_W_4
 M5 = W5 * X_W_5
+
+y_a1=
+y_a2=
+y_a3=
+M_overturning=
 
 
 
