39b6d03f3edafeff463db597616acc6c96705bfc
oralsiraj-coder <o.r.alsiraj@gmail.com>
2026-05-28 14:27:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -608,29 +608,16 @@ st.header("Earth Pressure Resultants")
 # --------------------------------------------------
 st.subheader("1. Horizontal force due to soil (Pa₁)")    
 Pa1 = 0.5 * Ka * gamma_a * Ha**2
-st.latex(r"P_{a1} = \frac{1}{2} K_a \gamma_a H_a^2")      
-st.latex( rf"P_{{a1}} = \frac{{1}}{{2}} \cdot {Ka:.3f} \cdot {gamma_a:.1f} \cdot {Ha:.1f}^2 = {Pa1:.2f}")
-st.latex(rf"P_{{a1}} = {Pa1:.2f}")
-st.latex(r"y_{a1} = \frac{H_a}{3}")
-st.latex(rf"y_{{a1}} = \frac{{{Ha:.1f}}}{{3}}")
-st.latex(rf"y_{{a1}} = {Ha/3:.2f}")
-#===================
-Pa1 = 0.5 * Ka * gamma_a * Ha**2
 ya1 = Ha / 3
-
 st.latex(
     rf"P_{{a1}} = \frac{{1}}{{2}} K_a \gamma_a H_a^2"
     rf" = \frac{{1}}{{2}} \cdot {Ka:.3f} \cdot {gamma_a:.1f} \cdot {Ha:.1f}^2"
     rf" = {Pa1:.2f}"
     rf"\quad,\quad ")
-
 st.latex(
     rf"y_{{a1}} = \frac{{H_a}}{{3}}"
     rf" = \frac{{{Ha:.1f}}}{{3}}"
     rf" = {ya1:.2f}" )
-
-
-
 # --------------------------------------------------
 # Pa2 – Water
 # --------------------------------------------------
