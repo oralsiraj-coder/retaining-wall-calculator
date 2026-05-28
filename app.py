9cb2d79acf050b32bdce683cad4a606eb0a81058
oralsiraj-coder <o.r.alsiraj@gmail.com>
2026-05-28 14:26:10 +0200

    Fix formatting in latex output for app.py

--- app.py
+++ app.py
@@ -622,11 +622,12 @@ st.latex(
     rf"P_{{a1}} = \frac{{1}}{{2}} K_a \gamma_a H_a^2"
     rf" = \frac{{1}}{{2}} \cdot {Ka:.3f} \cdot {gamma_a:.1f} \cdot {Ha:.1f}^2"
     rf" = {Pa1:.2f}"
-    rf"\quad,\quad "
+    rf"\quad,\quad ")
+
+st.latex(
     rf"y_{{a1}} = \frac{{H_a}}{{3}}"
     rf" = \frac{{{Ha:.1f}}}{{3}}"
-    rf" = {ya1:.2f}"
-)
+    rf" = {ya1:.2f}" )
 
 
 
