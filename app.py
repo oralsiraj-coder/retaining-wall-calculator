1ed1c42b90cee93be224c474c53864aeb91939f6
oralsiraj-coder <o.r.alsiraj@gmail.com>
2026-06-01 10:48:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -238,7 +238,7 @@ def draw_wall(Ha, Hw, Hp, Th, Lh, Lt, Tsb, beta,
 import os
 
 BASE_DIR = os.path.dirname(__file__)
-LOGO_PATH = os.path.join(BASE_DIR, "assets", "logo.png")
+LOGO_PATH = os.path.join(BASE_DIR, "assets", "logo.jpg")
 
 # Show logo in sidebar
 if os.path.exists(LOGO_PATH):
